dd61143616cd1aa9ed0f6a6f3f6723e42080111c
James Price <jamesmpdev@gmail.com>
2021-05-25 14:40:05 +0100

    adding edit and update ability

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -20,6 +20,17 @@ def create
     redirect_to tasks_path
 end
 
+def edit 
+    @task = Task.find(params[:id])
+end
+
+def update
+    @task = Task.find(params[:id])
+
+    @task.update(task_params)
+    redirect_to task_path
+end
+
 private
 
 def task_params
--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,14 @@ Rails.application.routes.draw do
   get "tasks", to: "tasks#index"
   
   # add new task
-  get "tasks/new", to: "tasks#new"
+  get "tasks/new", to: "tasks#new", as: :task_new
   post "tasks", to: "tasks#create"
+
+  # edit task
+  get "tasks/:id/edit", to: "tasks#edit", as: :task_edit
+  patch "tasks/:id", to: "tasks#update"
 
   # show page
   get "tasks/:id", to: "tasks#show", as: :task
-  
 
 end
