b34050de64bad6e07f64400a4ded721eece1c88b
James Price <jamesmpdev@gmail.com>
2021-05-25 15:19:25 +0100

    simple form working

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  # all tasks
-  get "tasks", to: "tasks#index"
+  # # all tasks
+  # get "tasks", to: "tasks#index"
   
-  # add new task
-  get "tasks/new", to: "tasks#new", as: :task_new
-  post "tasks", to: "tasks#create"
+  # # add new task
+  # get "tasks/new", to: "tasks#new", as: :task_new
+  # post "tasks", to: "tasks#create"
 
-  # edit task
-  get "tasks/:id/edit", to: "tasks#edit", as: :task_edit
-  patch "tasks/:id", to: "tasks#update"
+  # # edit task
+  # get "tasks/:id/edit", to: "tasks#edit", as: :task_edit
+  # patch "tasks/:id", to: "tasks#update"
 
-  # show page
-  get "tasks/:id", to: "tasks#show", as: :task
+  # # show page
+  # get "tasks/:id", to: "tasks#show", as: :task
 
+  # # delete task
+  # delete "tasks/:id", to: "tasks#destroy"
+
+  resources :tasks
 end
